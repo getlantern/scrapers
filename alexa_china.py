01797e16ee9ea4a62e898c4460d70aef26dff394
Ox Cart <ox.to.a.cart@gmail.com>
2014-09-19 14:08:52 -0500

    initial checkin

---
+++ alexa_china.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# This script prints out a JSON array of the Alexa top 500 sites in China.
+#
+# It requires the requests and lxml packages:
+#
+#   pip install requests
+#   pip install lxml
+
+import requests
+import lxml.html
+
+print "["
+for i in range(0,21):
+    html = requests.get("http://www.alexa.com/topsites/countries;%d/CN" % i).content
+    dom = lxml.html.fromstring(html)
+    
+    for link in dom.cssselect('.site-listing .desc-paragraph a'):
+        print '"%s,"' % link.text_content()
+
+print "]"   
